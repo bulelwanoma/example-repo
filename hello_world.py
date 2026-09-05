33ec6fd8aaaf4e62094686d7bf8fab83aacc6971
Bulelwa Noma <bulelwanoma@gmail.com>
2026-09-05 14:40:20 +0200

    Add_Hello_World_program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#Ask the user to capture their name using input() function and then print out the name using print() function 
+
+name=input("Please enter your name: ")
+print(name)
+ 
+
+#Ask the user to capture their age using the input()function and then print out age using print() function 
+
+age=input("Please enter your age: ")
+
+#Print "Hello World" using the print() function 
+print("Hello World")
+
